c1f047edf7727dae7723af65cdc232d601de5476
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 23:19:50 +0530

    Create While_Loop.c

---
+++ While_Loop.c
@@ -0,0 +1,17 @@
+//Created by: Vaidehi Muley
+//****While Loop****//
+
+#include <stdio.h>
+
+int main() {
+
+int count = 1;
+
+while (count <= 10) {
+    printf("\n%d", count);
+    count = ++count;
+}
+printf("\nWhile loop terminated");
+
+    return 0;
+}
