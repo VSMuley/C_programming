d31161614250512d65e23ba912935bd908b5b39a
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 09:29:30 +0530

    Create Integer Variable_Segmentation Fault.c

---
+++ Integer Variable_Segmentation Fault.c
@@ -0,0 +1,13 @@
+//Created by: Vaidehi Muley
+//****Integer Datatype and Segmentation Fault****//
+
+#include <stdio.h>
+
+int main() {
+  
+  int var = 45;
+  printf("%d\n", var);
+  printf("%b\n", var);
+  printf(var);
+  return 0; 
+}
