1e6492a7add326c2735fc6238e6174dadece6abd
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 23:36:51 +0530

    Create For_Loop.c

---
+++ For_Loop.c
@@ -0,0 +1,14 @@
+//Created by: Vaidehi Muley
+//****Sum using For Loop****//
+
+#include <stdio.h>
+
+int main() {
+
+int sum = 0;
+for (int i = 0; i <= 100; i++) {
+    sum = sum + i;
+}
+printf("\n%d",sum);
+    return 0;
+}
