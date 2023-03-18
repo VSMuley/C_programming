b7e8b91eafde2a97a194f064752e058c5a699baf
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 22:01:18 +0530

    Create Ternary_Operator.c

---
+++ Ternary_Operator.c
@@ -0,0 +1,16 @@
+//Created by: Vaidehi Muley
+//****Ternary Operator****//
+#include <stdio.h>
+
+
+int main() {
+
+int num;
+
+printf("Enter the number: "); //Display if entered number is even or odd
+scanf("\n%d",&num );
+
+(num%2 == 0) ? printf("Number entered is even") : printf("Number entered is odd");
+
+    return 0;
+}
