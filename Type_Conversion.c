06a1c8e7f0caeb82d1536838d8836f9ea1782a9f
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 19:32:07 +0530

    Update Type_Conversion.c

--- Type_Conversion.c
+++ Type_Conversion.c
@@ -10,8 +10,9 @@ int main() {
     int result1 = a + b;
     double result2 = a + b;
     float result3 = a + b;
+    double result4 = (int)a + b; //Explicit Type Conversion
     
-    printf(" %d\n %lf\n %f", result1, result2, result3);
+    printf(" %d\n %lf\n %f\n %lf", result1, result2, result3, result4);
 
     return 0;
 }
