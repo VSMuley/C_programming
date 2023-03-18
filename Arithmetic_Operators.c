64e6de24d7011388d22ac8602e3be8db22dfc923
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 18:57:41 +0530

    Update Arithmetic_Operators.c

--- Arithmetic_Operators.c
+++ Arithmetic_Operators.c
@@ -6,21 +6,23 @@ int main() {
     
     float x = 12.1, y = 3.4;
     int a = 4, b = 3;
-    float division, modulus, add, sub, mult, incr, decr;
+    float division, modulus, add, sub, mult, incr, decr, z;
     int divi, mod;
     
-    divi = x/y;
-    division = x/y;
+    divi = x/y; //Integer value
+    division = x/y; 
     modulus = a % b;
-    mod = a % b;
+    mod = a % b; // Integer value
     add = x + y; 
     sub = x - y; 
     mult = x * y; 
     incr = ++x; 
     decr = --y; 
     
+    z = 20 - 3 / 5 + 4 % 3; //Operator precedance
     
     printf("%d\n %f\n %d\n %f\n %f\n %f\n %f\n %f\n %f\n", divi,division, mod, modulus, add, sub, mult, incr, decr);
+    printf("%f", z);
 
     return 0;
 }
