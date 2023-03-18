b2b5024f3646eac61503ae97531e59efe10b641c
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 16:05:19 +0530

    Create Datatypes.c

---
+++ Datatypes.c
@@ -0,0 +1,19 @@
+//Created by: Vaidehi Muley
+//****Datatypes in C****//
+
+#include <stdio.h>
+
+int main() {
+  
+ char character = 'z';  //Strings in C
+ float f = 3.4;
+ double df = 56.5;
+ 
+ printf("%c\n", character); 
+ printf("%d\n", character); //Integer equivalent value of the string
+ printf("%f\n", f);
+ printf("%.3f\n", f);
+ printf("%lf\n", df);
+  
+  return 0; 
+}
