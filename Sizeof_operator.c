72b1205317e449e0b73815ecda14070a1e32e04f
VSMuley <126000783+VSMuley@users.noreply.github.com>
2023-03-18 16:10:57 +0530

    Create Sizeof_operator.c

---
+++ Sizeof_operator.c
@@ -0,0 +1,20 @@
+//Created by: Vaidehi Muley
+
+//****Sizeof Operator****//
+
+#include <stdio.h>
+
+int main() {
+  
+ int age;
+ char character;
+ float f;
+ double df;
+ 
+ printf ("Size of variable age: %zu\n", sizeof(age));
+ printf ("Size of variable character: %zu\n", sizeof(character));
+ printf ("Size of variable f: %zu\n", sizeof(f));
+ printf ("Size of variable df: %zu\n", sizeof(df));
+ 
+  return 0; 
+}
